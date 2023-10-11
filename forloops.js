f903e2cc7973912865794a7b8ab9ce6655d03d69
Rithishkumar.J <rithishkumarj@dckap.com>
2023-10-11 22:27:20 +0530

    for loop file added

---
+++ forloops.js
@@ -0,0 +1,98 @@
+"use strict";
+
+
+// ----------------- store in new array ------------------
+
+
+// let arr = [23,7,189,88,1902,1];
+
+// // console.log(arr[6])
+// let newArr = [];
+
+// // newArr = arr;
+
+// // let count = 0;
+// for(let i=arr.length-1 ; i >=0 ; i--)
+// {
+//    newArr[arr.length-1-i] = arr[i];
+  
+//    // newArr[0] = arr[0]
+//    // newArr[1] = arr[1]
+//    //...
+//    //..
+//    // newArr[5] = arr[5]
+// }
+
+// console.log(newArr);
+
+
+// ----------------- pattern -----------------
+
+// *****
+// ****
+// ***
+// **
+// *
+
+// let n=5;
+// let p="";
+// for(let i=1;i<=n;i++){
+//     for(let j=1;j<=i;j++){
+//       p+=""+j;
+//     }
+//     p+="\n"
+//     console.log(p);
+// }
+// console.log(p);
+
+
+// ----------------- ascending order -----------------
+
+// let arr=[23,13,45,67,89,2,29,8]
+
+// for(var i=1;i<arr.length;i++)
+// {
+//     for(var j=0;j<i;j++)
+//     {
+//         if(arr[i]<arr[j])
+//         {
+//             let x=arr[i];
+//             arr[i]=arr[j];
+//             arr[j]=x
+//         }
+//         console.log(arr)
+//     }
+// }
+
+
+// --------------  pattern 2 -------------------
+
+// 5 4 3 2 1 
+// 4 3 2 1 
+// 3 2 1 
+// 2 1 
+// 1
+
+
+// let n=5;
+
+// for(var i=n;i>=1;i--)
+// {
+//     var x='';
+//     for(var j=i;j>=1;j--)
+//     {
+       
+//         x+=j+" "
+//     }
+//     console.log(x)
+// }
+
+
+
+
+
+
+
+
+
+
